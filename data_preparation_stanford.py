283495407d748d0e6fba40eb844a0af0ea8df2c3
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-04-12 16:44:33 +0200

    Saving stan's data

--- data_preparation_stanford.py
+++ data_preparation_stanford.py
@@ -1,3 +1,4 @@
+import pickle
 import cv2
 from sklearn.model_selection import train_test_split
 from collections import Counter
@@ -7,9 +8,11 @@ import io
 import matplotlib.pyplot as plt
 
 DOWNLOAD = False  # Set to True to download the data
-IMAGE_N = 234  # change this to a number between [0, 1200] and you can see a different training image
-TEST_SIZE = 0.1
+IMAGE_N = 234  # change this to a number between [0, 1200] and you can see a different training image, None for no image
+TEST_SIZE = 0.1  # Proportion of all data to use for testing
+VAL_SIZE = 0.1  # Proportion of the training set to use for validation
 RANDOM_STATE = 0
+pickle_location = "./data/Stanford40.pickle"  # Location to save the data to
 
 keep_stanford40 = ["applauding", "climbing", "drinking", "jumping", "pouring_liquid", "riding_a_bike", "riding_a_horse",
                    "running", "shooting_an_arrow", "smoking", "throwing_frisby", "waving_hands"]
@@ -38,24 +41,40 @@ with open("Stanford40/ImageSplits/test.txt", "r") as f:
 # Combine the splits and split for keeping more images in the training set than the test set.
 all_files = train_files + test_files
 all_labels = train_labels + test_labels
-train_files, test_files = train_test_split(all_files, test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=all_labels)
+remainder_files, test_files = train_test_split(all_files, test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=all_labels)
+
+remainder_labels = ["_".join(name.split("_")[:-1]) for name in remainder_files]  # Get the labels for the remainder
+train_files, val_files = train_test_split(remainder_files, test_size=VAL_SIZE, random_state=RANDOM_STATE, stratify=remainder_labels)
+
 train_labels = ["_".join(name.split("_")[:-1]) for name in train_files]
 test_labels = ["_".join(name.split("_")[:-1]) for name in test_files]
+val_labels = ["_".join(name.split("_")[:-1]) for name in val_files]
 
-print(f"Train files ({len(train_files)}):\n\t{train_files}")
-print(f"Train labels ({len(train_labels)}):\n\t{train_labels}\n"
-      f"Train Distribution:{list(Counter(sorted(train_labels)).items())}\n")
+print(f"Train files ({len(train_files)})")
+print(f"Train Distribution:{list(Counter(sorted(train_labels)).items())}\n")
 
-print(f"Test files ({len(test_files)}):\n\t{test_files}")
-print(f"Test labels ({len(test_labels)}):\n\t{test_labels}\n"
-      f"Test Distribution:{list(Counter(sorted(test_labels)).items())}\n")
+print(f"Test files ({len(test_files)})")
+print(f"Test Distribution:{list(Counter(sorted(test_labels)).items())}\n")
+
+print(f"Validation files ({len(val_files)})")
+print(f"Validation Distribution:{list(Counter(sorted(val_labels)).items())}\n")
 
 action_categories = sorted(list(set(train_labels)))
 print(f"Action categories ({len(action_categories)}):\n{action_categories}")
 
-img = cv2.imread(f"./Stanford40/JPEGImages/{train_files[IMAGE_N]}")
-print(f"An image with the label - {train_labels[IMAGE_N]}")
+# Save data to pickle file
+data = {"train_files": train_files, "train_labels": train_labels,
+        "test_files": test_files, "test_labels": test_labels,
+        "val_files": val_files, "val_labels": val_labels,
+        "action_categories": action_categories}
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-plt.imshow(img)  # Show image with matplotlib
-plt.show()
+with open(pickle_location, "wb") as f:
+    pickle.dump(data, f)
+
+if IMAGE_N is not None:  # Show an image if IMAGE_N is not None
+    img = cv2.imread(f"./Stanford40/JPEGImages/{train_files[IMAGE_N]}")
+    print(f"An image with the label: {train_labels[IMAGE_N]}")
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+    plt.imshow(img)  # Show image with matplotlib
+    plt.show()

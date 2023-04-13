9bbef86034d50dad19de5ce6d142b581a0ba42cf
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-04-13 17:40:43 +0200

    Made list of classes available for other files

--- data_preparation_HMDB.py
+++ data_preparation_HMDB.py
@@ -8,92 +8,92 @@ import patoolib
 import requests
 from sklearn.model_selection import train_test_split
 
-DOWNLOAD = False  # Set to True to download the data
-EXTRACT = False  # Set to True to extract the data
-VAL_SIZE = 0.1  # Proportion of the training set to use for validation
-RANDOM_STATE = 0
-pickle_location = "./data/hmdb.pickle"  # Location to save the data to
-
 keep_hmdb51 = ["clap", "climb", "drink", "jump", "pour", "ride_bike", "ride_horse",
                "run", "shoot_bow", "smoke", "throw", "wave"]
 
+if __name__ == "__main__":
+    DOWNLOAD = False  # Set to True to download the data
+    EXTRACT = False  # Set to True to extract the data
+    VAL_SIZE = 0.1  # Proportion of the training set to use for validation
+    RANDOM_STATE = 0
+    pickle_location = "./data/hmdb.pickle"  # Location to save the data to
 
-# Use requests to download the data
-if DOWNLOAD:
-    # Make dirs
-    for dir in "video_data", "test_train_splits":
-        os.makedirs(dir, exist_ok=True)
+    # Use requests to download the data
+    if DOWNLOAD:
+        # Make dirs
+        for dir in "video_data", "test_train_splits":
+            os.makedirs(dir, exist_ok=True)
 
-    videos = "http://serre-lab.clps.brown.edu/wp-content/uploads/2013/10/hmdb51_org.rar"
-    splits = "http://serre-lab.clps.brown.edu/wp-content/uploads/2013/10/test_train_splits.rar"
+        videos = "http://serre-lab.clps.brown.edu/wp-content/uploads/2013/10/hmdb51_org.rar"
+        splits = "http://serre-lab.clps.brown.edu/wp-content/uploads/2013/10/test_train_splits.rar"
 
-    for url in (videos, splits):
-        path = "./data/" + url.split("/")[-1]
+        for url in (videos, splits):
+            path = "./data/" + url.split("/")[-1]
 
-        r = requests.get(url)
-        with open(path, "wb") as f:  # Save rars to disk
-            f.write(r.content)
+            r = requests.get(url)
+            with open(path, "wb") as f:  # Save rars to disk
+                f.write(r.content)
 
-if EXTRACT:  # Extract the rar files
-    patoolib.extract_archive("./data/hmdb51_org.rar", outdir="video_data")
-    patoolib.extract_archive("./data/test_train_splits.rar", outdir="test_train_splits")
+    if EXTRACT:  # Extract the rar files
+        patoolib.extract_archive("./data/hmdb51_org.rar", outdir="video_data")
+        patoolib.extract_archive("./data/test_train_splits.rar", outdir="test_train_splits")
 
-    for files in os.listdir("video_data"):
-        foldername = files.split(".")[0]
-        if foldername in keep_hmdb51:  # Extract only the relevant classes for the assignment.
-            Path(f"video_data/{foldername}").mkdir(parents=True, exist_ok=True)
-            patoolib.extract_archive(f"video_data/{files}", outdir=f"video_data/{foldername}")
+        for files in os.listdir("video_data"):
+            foldername = files.split(".")[0]
+            if foldername in keep_hmdb51:  # Extract only the relevant classes for the assignment.
+                Path(f"video_data/{foldername}").mkdir(parents=True, exist_ok=True)
+                patoolib.extract_archive(f"video_data/{files}", outdir=f"video_data/{foldername}")
 
-TRAIN_TAG, TEST_TAG = 1, 2
-train_files, test_files = [], []
-train_labels, test_labels = [], []
-split_pattern_name = f"*test_split1.txt"
-split_pattern_path = os.path.join("test_train_splits", "testTrainMulti_7030_splits", split_pattern_name)
-annotation_paths = glob.glob(split_pattern_path)
-print(f"Found {len(annotation_paths)} annotation files.")
+    TRAIN_TAG, TEST_TAG = 1, 2
+    train_files, test_files = [], []
+    train_labels, test_labels = [], []
+    split_pattern_name = f"*test_split1.txt"
+    split_pattern_path = os.path.join("test_train_splits", "testTrainMulti_7030_splits", split_pattern_name)
+    annotation_paths = glob.glob(split_pattern_path)
+    print(f"Found {len(annotation_paths)} annotation files.")
 
-for filepath in annotation_paths:
-    class_name = "_".join(filepath.split(os.sep)[-1].split("_")[:-2])  # os.sep is the path separator for the current OS.
+    for filepath in annotation_paths:
+        class_name = "_".join(filepath.split(os.sep)[-1].split("_")[:-2])  # os.sep is the path separator for the current OS.
 
-    if class_name not in keep_hmdb51:
-        continue  # skipping the classes that we won't use.
+        if class_name not in keep_hmdb51:
+            continue  # skipping the classes that we won't use.
 
-    with open(filepath) as fid:
-        lines = fid.readlines()
+        with open(filepath) as fid:
+            lines = fid.readlines()
 
-    for line in lines:
-        video_filename, tag_string = line.split()
-        tag = int(tag_string)
+        for line in lines:
+            video_filename, tag_string = line.split()
+            tag = int(tag_string)
 
-        if tag == TRAIN_TAG:
-            train_files.append(video_filename)
-            train_labels.append(class_name)
+            if tag == TRAIN_TAG:
+                train_files.append(video_filename)
+                train_labels.append(class_name)
 
-        elif tag == TEST_TAG:
-            test_files.append(video_filename)
-            test_labels.append(class_name)
+            elif tag == TEST_TAG:
+                test_files.append(video_filename)
+                test_labels.append(class_name)
 
-# Split the train data into train and validation sets.
-train_files, val_files, train_labels, val_labels = train_test_split(train_files, train_labels,
-                                                                    test_size=VAL_SIZE, random_state=RANDOM_STATE,
-                                                                    stratify=train_labels)
+    # Split the train data into train and validation sets.
+    train_files, val_files, train_labels, val_labels = train_test_split(train_files, train_labels,
+                                                                        test_size=VAL_SIZE, random_state=RANDOM_STATE,
+                                                                        stratify=train_labels)
 
-print(f"Train files ({len(train_files)})")
-print(f"Train Distribution:{list(Counter(sorted(train_labels)).items())}")
+    print(f"Train files ({len(train_files)})")
+    print(f"Train Distribution:{list(Counter(sorted(train_labels)).items())}")
 
-print(f"Validation files ({len(val_files)})")
-print(f"Validation Distribution:{list(Counter(sorted(val_labels)).items())}")
+    print(f"Validation files ({len(val_files)})")
+    print(f"Validation Distribution:{list(Counter(sorted(val_labels)).items())}")
 
-print(f"Test files ({len(test_files)})")
-print(f"Test Distribution:{list(Counter(sorted(test_labels)).items())}")
+    print(f"Test files ({len(test_files)})")
+    print(f"Test Distribution:{list(Counter(sorted(test_labels)).items())}")
 
-action_categories = sorted(list(set(train_labels)))
-print(f"Action categories ({len(action_categories)}):\n{action_categories}")
+    action_categories = sorted(list(set(train_labels)))
+    print(f"Action categories ({len(action_categories)}):\n{action_categories}")
 
-data = {"train_files": train_files, "train_labels": train_labels,
-        "val_files": val_files, "val_labels": val_labels,
-        "test_files": test_files, "test_labels": test_labels,
-        "action_categories": action_categories}
+    data = {"train_files": train_files, "train_labels": train_labels,
+            "val_files": val_files, "val_labels": val_labels,
+            "test_files": test_files, "test_labels": test_labels,
+            "action_categories": action_categories}
 
-with open(pickle_location, "wb") as f:
-    pickle.dump(data, f)
+    with open(pickle_location, "wb") as f:
+        pickle.dump(data, f)
--- data_preparation_stanford.py
+++ data_preparation_stanford.py
@@ -8,74 +8,76 @@ import matplotlib.pyplot as plt
 import requests
 from sklearn.model_selection import train_test_split
 
-DOWNLOAD = False  # Set to True to download the data
-IMAGE_N = 234  # change this to a number between [0, 1200] and you can see a different training image, None for no image
-TEST_SIZE = 0.1  # Proportion of all data to use for testing
-VAL_SIZE = 0.1  # Proportion of the training set to use for validation
-RANDOM_STATE = 0
-pickle_location = "./data/Stanford40.pickle"  # Location to save the data to
-
 keep_stanford40 = ["applauding", "climbing", "drinking", "jumping", "pouring_liquid", "riding_a_bike", "riding_a_horse",
                    "running", "shooting_an_arrow", "smoking", "throwing_frisby", "waving_hands"]
 
-if DOWNLOAD:  # Download the data if it is not already downloaded
-    jpeg = "http://vision.stanford.edu/Datasets/Stanford40_JPEGImages.zip"
-    splits = "http://vision.stanford.edu/Datasets/Stanford40_ImageSplits.zip"
+if __name__ == "__main__":
+    DOWNLOAD = False  # Set to True to download the data
+    IMAGE_N = 234  # change this to a number between [0, 1200] and you can see a different training image, None for no image
+    TEST_SIZE = 0.1  # Proportion of all data to use for testing
+    VAL_SIZE = 0.1  # Proportion of the training set to use for validation
+    RANDOM_STATE = 0
+    pickle_location = "./data/Stanford40.pickle"  # Location to save the data to
 
-    # Download the file from the url and save it locally under Stanford40/
-    for url in (jpeg, splits):
-        r = requests.get(url)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall("Stanford40")
+    if DOWNLOAD:  # Download the data if it is not already downloaded
+        jpeg = "http://vision.stanford.edu/Datasets/Stanford40_JPEGImages.zip"
+        splits = "http://vision.stanford.edu/Datasets/Stanford40_ImageSplits.zip"
 
+        # Download the file from the url and save it locally under Stanford40/
+        for url in (jpeg, splits):
+            r = requests.get(url)
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall("Stanford40")
 
-with open("./Stanford40/ImageSplits/train.txt", "r") as f:
-    # We won't use these splits but split them ourselves
-    train_files = [file_name for file_name in list(map(str.strip, f.readlines())) if "_".join(file_name.split("_")[:-1]) in keep_stanford40]
+    with open("./Stanford40/ImageSplits/train.txt", "r") as f:
+        # We won't use these splits but split them ourselves
+        train_files = [file_name for file_name in list(map(str.strip, f.readlines()))
+                       if "_".join(file_name.split("_")[:-1]) in keep_stanford40]
+        train_labels = ["_".join(name.split("_")[:-1]) for name in train_files]
+
+    with open("Stanford40/ImageSplits/test.txt", "r") as f:
+        # We won't use these splits but split them ourselves
+        test_files = [file_name for file_name in list(map(str.strip, f.readlines()))
+                      if "_".join(file_name.split("_")[:-1]) in keep_stanford40]
+        test_labels = ["_".join(name.split("_")[:-1]) for name in test_files]
+
+    # Combine the splits and split for keeping more images in the training set than the test set.
+    all_files = train_files + test_files
+    all_labels = train_labels + test_labels
+    remainder_files, test_files = train_test_split(all_files, test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=all_labels)
+
+    remainder_labels = ["_".join(name.split("_")[:-1]) for name in remainder_files]  # Get the labels for the remainder
+    train_files, val_files = train_test_split(remainder_files, test_size=VAL_SIZE, random_state=RANDOM_STATE, stratify=remainder_labels)
+
     train_labels = ["_".join(name.split("_")[:-1]) for name in train_files]
-
-with open("Stanford40/ImageSplits/test.txt", "r") as f:
-    # We won't use these splits but split them ourselves
-    test_files = [file_name for file_name in list(map(str.strip, f.readlines())) if "_".join(file_name.split("_")[:-1]) in keep_stanford40]
     test_labels = ["_".join(name.split("_")[:-1]) for name in test_files]
+    val_labels = ["_".join(name.split("_")[:-1]) for name in val_files]
 
-# Combine the splits and split for keeping more images in the training set than the test set.
-all_files = train_files + test_files
-all_labels = train_labels + test_labels
-remainder_files, test_files = train_test_split(all_files, test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=all_labels)
+    print(f"Train files ({len(train_files)})")
+    print(f"Train Distribution:{list(Counter(sorted(train_labels)).items())}\n")
 
-remainder_labels = ["_".join(name.split("_")[:-1]) for name in remainder_files]  # Get the labels for the remainder
-train_files, val_files = train_test_split(remainder_files, test_size=VAL_SIZE, random_state=RANDOM_STATE, stratify=remainder_labels)
+    print(f"Validation files ({len(val_files)})")
+    print(f"Validation Distribution:{list(Counter(sorted(val_labels)).items())}\n")
 
-train_labels = ["_".join(name.split("_")[:-1]) for name in train_files]
-test_labels = ["_".join(name.split("_")[:-1]) for name in test_files]
-val_labels = ["_".join(name.split("_")[:-1]) for name in val_files]
+    print(f"Test files ({len(test_files)})")
+    print(f"Test Distribution:{list(Counter(sorted(test_labels)).items())}\n")
 
-print(f"Train files ({len(train_files)})")
-print(f"Train Distribution:{list(Counter(sorted(train_labels)).items())}\n")
+    action_categories = sorted(list(set(train_labels)))
+    print(f"Action categories ({len(action_categories)}):\n{action_categories}")
 
-print(f"Validation files ({len(val_files)})")
-print(f"Validation Distribution:{list(Counter(sorted(val_labels)).items())}\n")
+    # Save data to pickle file
+    data = {"train_files": train_files, "train_labels": train_labels,
+            "test_files": test_files, "test_labels": test_labels,
+            "val_files": val_files, "val_labels": val_labels,
+            "action_categories": action_categories}
 
-print(f"Test files ({len(test_files)})")
-print(f"Test Distribution:{list(Counter(sorted(test_labels)).items())}\n")
+    with open(pickle_location, "wb") as f:
+        pickle.dump(data, f)
 
-action_categories = sorted(list(set(train_labels)))
-print(f"Action categories ({len(action_categories)}):\n{action_categories}")
+    if IMAGE_N is not None:  # Show an image if IMAGE_N is not None
+        img = cv2.imread(f"./Stanford40/JPEGImages/{train_files[IMAGE_N]}")
+        print(f"An image with the label: {train_labels[IMAGE_N]}")
 
-# Save data to pickle file
-data = {"train_files": train_files, "train_labels": train_labels,
-        "test_files": test_files, "test_labels": test_labels,
-        "val_files": val_files, "val_labels": val_labels,
-        "action_categories": action_categories}
-
-with open(pickle_location, "wb") as f:
-    pickle.dump(data, f)
-
-if IMAGE_N is not None:  # Show an image if IMAGE_N is not None
-    img = cv2.imread(f"./Stanford40/JPEGImages/{train_files[IMAGE_N]}")
-    print(f"An image with the label: {train_labels[IMAGE_N]}")
-
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-    plt.imshow(img)  # Show image with matplotlib
-    plt.show()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+        plt.imshow(img)  # Show image with matplotlib
+        plt.show()

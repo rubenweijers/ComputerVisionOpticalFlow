048c6c0ecccdd9b5a278fa924c86824949775471
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-04-13 17:19:13 +0200

    Added possibility for deepflow

--- train_models_hmdb.py
+++ train_models_hmdb.py
@@ -23,18 +23,46 @@ def load_data(filepath: str = "data/hmdb.pickle"):
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
-def load_video(filename: str, label: str, frame_n: int = "halfway"):
-    """Load a video from a filepath and return a single frame from it."""
+def load_video(filename: str, label: str, frame_n: int = "halfway", greyscale: bool = True, resize=(112, 112)):
+    """Load a video from a filepath and return two consecutive frames.
+
+    Set frame_n to "halfway" to get the middle frame, or an integer to get a specific frame.
+    Set resize to None to not resize the image, or a tuple to resize the image.
+    """
     fp = os.path.join("./video_data", label, label, filename)  # Class label twice due to folder structure
     cap = cv2.VideoCapture(fp)
 
     if frame_n == "halfway":  # Exception for the middle frame
         frame_n = int(cap.get(cv2.CAP_PROP_FRAME_COUNT) / 2)  # Get the middle frame
-
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_n)  # Set the frame number
-    ret, img = cap.read()  # Only read the frame we want
 
-    return img
+    imgs = []
+    for _ in range(2):  # Get the next two frames
+        ret, img = cap.read()  # Only read the frame we want
+
+        if ret:  # If the frame was read successfully
+            if greyscale:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Greyscale the image
+            if resize is not None:
+                img = cv2.resize(img, resize)  # Resize the image
+            imgs.append(img)
+
+    cap.release()
+
+    if len(imgs) != 2:
+        raise ValueError(f"Could not load two frames from: {filename}")
+
+    return imgs
+
+
+def deepflow(img1, img2):
+    """Calculate the optical flow between two images using the DeepFlow algorithm.
+    Based on: https://gist.github.com/FingerRec/eba088d6d7a50e17c875d74684ec2849
+    """
+    deepflow = cv2.optflow.createOptFlow_DeepFlow()
+    result = deepflow.calc(img1, img2, None)
+
+    return result
 
 
 if __name__ == "__main__":
@@ -43,11 +71,16 @@ if __name__ == "__main__":
     print(f"{X_train.shape=}; {y_train.shape=}; {X_val.shape=}; {y_val.shape=}; {X_test.shape=}; {y_test.shape=}")
 
     # Convert filepaths to images
-    X_train = [load_video(filepath, label) for filepath, label in tzip(X_train, y_train, desc="Loading X_train data")]
-    X_val = [load_video(filepath, label) for filepath, label in tzip(X_val, y_val, desc="Loading X_val data")]
-    X_test = [load_video(filepath, label) for filepath, label in tzip(X_test, y_test, desc="Loading X_test data")]
+    X_train = [load_video(filepath, label) for filepath, label in tzip(X_train[:10], y_train[:10], desc="Loading X_train data")]
+    # X_val = [load_video(filepath, label) for filepath, label in tzip(X_val, y_val, desc="Loading X_val data")]
+    # X_test = [load_video(filepath, label) for filepath, label in tzip(X_test, y_test, desc="Loading X_test data")]
 
-    print(f"{X_train[0].shape=}; {X_val[0].shape=}; {X_test[0].shape=}")
+    print(f"{X_train[0][0].shape=}; {X_train[0][1].shape=}")
+
+    result = deepflow(X_train[0][0], X_train[0][1])
+    print(f"{result=}")
+    print(f"{result.shape=}")
+
     exit()
 
     kernel_size = 3
@@ -95,4 +128,5 @@ if __name__ == "__main__":
 
     # Evaluate the model
     loss, accuracy = model.evaluate(X_test, y_test)
+    print(f"Loss: {loss:.2f}; Accuracy: {accuracy:.2f}")
     print(f"Loss: {loss:.2f}; Accuracy: {accuracy:.2f}")

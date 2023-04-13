0d6729ce5a7eeba16fc42095027cafdbd9853aea
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-04-13 17:50:37 +0200

    Labels are now one-hot encoded

--- train_models_hmdb.py
+++ train_models_hmdb.py
@@ -4,6 +4,7 @@ import pickle
 import cv2
 import numpy as np
 from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.utils import to_categorical
 from tqdm.contrib import tzip
 
 from models import make_model, prepare_model
@@ -14,22 +15,40 @@ def load_data(filepath: str = "data/hmdb.pickle"):
         data = pickle.load(f)
 
     X_train = np.array(data["train_files"])
-    y_train = np.array(data["train_labels"])
     X_val = np.array(data["val_files"])
-    y_val = np.array(data["val_labels"])
     X_test = np.array(data["test_files"])
+
+    y_train = np.array(data["train_labels"])
+    y_val = np.array(data["val_labels"])
     y_test = np.array(data["test_labels"])
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    action_categories = data["action_categories"]
+    mapping = {i: label for i, label in enumerate(action_categories)}
+    print(f"Action categories ({len(action_categories)}):\n{action_categories}")
+
+    # Map the labels to integers based on index in list
+    # Action categories are in same order as Stanford40 dataset
+    y_train = np.array([action_categories.index(label) for label in y_train])
+    y_val = np.array([action_categories.index(label) for label in y_val])
+    y_test = np.array([action_categories.index(label) for label in y_test])
+
+    # Convert the labels to categorical
+    y_train = to_categorical(y_train, num_classes=len(action_categories))
+    y_val = to_categorical(y_val, num_classes=len(action_categories))
+    y_test = to_categorical(y_test, num_classes=len(action_categories))
+
+    # Create a mapping from integer to label
+    return X_train, y_train, X_val, y_val, X_test, y_test, mapping
 
 
-def load_video(filename: str, label: str, frame_n: int = "halfway", greyscale: bool = True, resize=(112, 112)):
+def load_video(filename: str, label: str, mapping: dict, frame_n: int = "halfway", greyscale: bool = True, resize=(112, 112)):
     """Load a video from a filepath and return two consecutive frames.
 
     Set frame_n to "halfway" to get the middle frame, or an integer to get a specific frame.
     Set resize to None to not resize the image, or a tuple to resize the image.
     """
-    fp = os.path.join("./video_data", label, label, filename)  # Class label twice due to folder structure
+    label_str = mapping[np.argmax(label)]  # Get the label string from the mapping
+    fp = os.path.join("./video_data", label_str, label_str, filename)  # Class label twice due to folder structure
     cap = cv2.VideoCapture(fp)
 
     if frame_n == "halfway":  # Exception for the middle frame
@@ -67,25 +86,40 @@ def deepflow(img1, img2):
 
 if __name__ == "__main__":
     # Load the data from the HMDB dataset
-    X_train, y_train, X_val, y_val, X_test, y_test = load_data("data/hmdb.pickle")
+    X_train, y_train, X_val, y_val, X_test, y_test, mapping = load_data("data/hmdb.pickle")
     print(f"{X_train.shape=}; {y_train.shape=}; {X_val.shape=}; {y_val.shape=}; {X_test.shape=}; {y_test.shape=}")
 
-    # Convert filepaths to images
-    X_train = [load_video(filepath, label) for filepath, label in tzip(X_train[:10], y_train[:10], desc="Loading X_train data")]
-    # X_val = [load_video(filepath, label) for filepath, label in tzip(X_val, y_val, desc="Loading X_val data")]
-    # X_test = [load_video(filepath, label) for filepath, label in tzip(X_test, y_test, desc="Loading X_test data")]
+    model_variation = "model2"  # Either {"model2", "model3"}
 
-    print(f"{X_train[0][0].shape=}; {X_train[0][1].shape=}")
+    if model_variation == "model2":  # Do not use optical flow, but use the middle frame
+        # Convert filepaths to images
+        X_train = [load_video(filepath, label, mapping, greyscale=False)
+                   for filepath, label in tzip(X_train, y_train, desc="Loading X_train data")]
+        X_val = [load_video(filepath, label, mapping, greyscale=False)
+                 for filepath, label in tzip(X_val, y_val, desc="Loading X_val data")]
+        # X_test = [load_video(filepath, label, mapping, greyscale=False)
+        #           for filepath, label in tzip(X_test, y_test, desc="Loading X_test data")]
 
-    result = deepflow(X_train[0][0], X_train[0][1])
-    print(f"{result=}")
-    print(f"{result.shape=}")
+        X_train = np.array([imgs[0] for imgs in X_train])  # Only use one frame
+        X_val = np.array([imgs[0] for imgs in X_val])
+        # X_test = np.array([imgs[0] for imgs in X_test])
 
-    exit()
+    elif model_variation == "model3":  # Use optical flow
+        # Convert filepaths to optical flow images
+        X_train = [deepflow(*load_video(filepath, label, greyscale=True))
+                   for filepath, label in tzip(X_train[:10], y_train[:10], desc="Loading X_train data")]
+        # X_val = [deepflow(*load_video(filepath, label, greyscale=True))
+        #          for filepath, label in tzip(X_val, y_val, desc="Loading X_val data")]
+        # X_test = [deepflow(*load_video(filepath, label, greyscale=True))
+        #           for filepath, label in tzip(X_test, y_test, desc="Loading X_test data")]
+
+        X_train = np.array(X_train)
+        # X_val = np.array(X_val)
+        # X_test = np.array(X_test)
 
     kernel_size = 3
     pool_size = 2
-    pooling_type = "max"
+    pooling_type = "avg"
     dropout_value = None
     conv_act = "relu"
     normalise = False
@@ -94,21 +128,6 @@ if __name__ == "__main__":
     batch_size = 64
     total_size = X_train.shape[0]
     epochs = 15
-
-    model_variation = "averagejoe"  # Choose from: {baseline, nike, collegedropout, normaliser2000, averagejoe}
-
-    if model_variation == "baseline":
-        pass  # Default values are already set
-    elif model_variation == "nike":
-        conv_act = "swish"
-    elif model_variation == "collegedropout":
-        dropout_value = 0.5
-    elif model_variation == "normaliser2000":
-        normalise = True
-    elif model_variation == "averagejoe":
-        pooling_type = "avg"
-    else:
-        raise ValueError(f"Invalid model variation: {model_variation}")
 
     # Create the model
     model = make_model(kernel_size=kernel_size, pool_size=pool_size, pooling_type=pooling_type,

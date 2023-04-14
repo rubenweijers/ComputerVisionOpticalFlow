8a54e25a2dcc0100eb0e324bd1d6c758e8c28cbf
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-04-14 12:25:35 +0200

    Added names and way to combine models

--- models.py
+++ models.py
@@ -7,7 +7,8 @@ from tensorflow.keras.optimizers import Adam
 
 
 def make_model(kernel_size: int = 3, pool_size: int = 2, pooling_type: str = "avg",
-               dropout_value: float = None, conv_act: str = "relu", normalise: bool = False, input_shape: tuple = (112, 112, 3)):
+               dropout_value: float = None, conv_act: str = "relu", normalise: bool = False, input_shape: tuple = (112, 112, 3),
+               model_variation: str = "model2") -> Sequential:
     """The model used for the experiments."""
 
     if pooling_type == "max":
@@ -26,31 +27,31 @@ def make_model(kernel_size: int = 3, pool_size: int = 2, pooling_type: str = "av
 
     # Input shape defined here is (112, 112, 3) because the images are resized to 112x112
     model = Sequential()
-    model.add(Conv2D(64, kernel_size, activation=conv_act, input_shape=input_shape))
+    model.add(Conv2D(64, kernel_size, activation=conv_act, input_shape=input_shape, name=f"{model_variation}_conv2d_1"))
 
     if normalise:
-        model.add(BatchNormalization())
+        model.add(BatchNormalization(name=f"{model_variation}_batchnorm_1"))
 
-    model.add(pooling(pool_size))
-    model.add(Conv2D(32, kernel_size, activation=conv_act))
-
-    if normalise:
-        model.add(BatchNormalization())
-
-    model.add(pooling(pool_size))
-    model.add(Conv2D(16, kernel_size, activation=conv_act))
+    model.add(pooling(pool_size, name=f"{model_variation}_pooling_1"))
+    model.add(Conv2D(32, kernel_size, activation=conv_act, name=f"{model_variation}_conv2d_2"))
 
     if normalise:
-        model.add(BatchNormalization())
+        model.add(BatchNormalization(name=f"{model_variation}_batchnorm_2"))
 
-    model.add(pooling(pool_size))
-    model.add(Flatten())
+    model.add(pooling(pool_size, name=f"{model_variation}_pooling_2"))
+    model.add(Conv2D(16, kernel_size, activation=conv_act, name=f"{model_variation}_conv2d_3"))
+
+    if normalise:
+        model.add(BatchNormalization(name=f"{model_variation}_batchnorm_3"))
+
+    model.add(pooling(pool_size, name=f"{model_variation}_pooling_3"))
+    model.add(Flatten(name=f"{model_variation}_flatten"))
 
     if dropout_value is not None:
-        model.add(Dropout(dropout_value))
+        model.add(Dropout(dropout_value, name=f"{model_variation}_dropout"))
 
-    model.add(Dense(128, activation="relu"))
-    model.add(Dense(12, activation="softmax"))
+    model.add(Dense(128, activation="relu", name=f"{model_variation}_dense_1"))
+    model.add(Dense(12, activation="softmax", name=f"{model_variation}_dense_2"))
     return model
 
 
@@ -63,7 +64,32 @@ def prepare_model(model, learning_rate: float = 0.01, batch_size: int = 64, tota
     return model
 
 
-# Create new class for cyclic learning rate
+def combine_models(model_frames, model_deepflow, n_classes: int = 12):
+    """Combine two models into one mode using late fusion and the functional API.
+    Remove the last layer of each model and add a new layer to the combined model."""
+    inputs = [model_frames.input, model_deepflow.input]
+
+    # Get the last layer of each model
+    outputs_frames = model_frames.layers[-2].output
+    outputs_deepflow = model_deepflow.layers[-2].output
+
+    # Concatenate the outputs of the two models
+    concat = tf.keras.layers.concatenate([outputs_frames, outputs_deepflow])
+
+    # Add dense layers
+    decoder = Dense(128, activation="relu", name="decoder_dense_1")(concat)
+    decoder = Dense(64, activation="relu", name="decoder_dense_2")(decoder)
+    decoder = Dense(32, activation="relu", name="decoder_dense_3")(decoder)
+
+    # Add the final layer
+    outputs = Dense(n_classes, activation="softmax")(decoder)
+
+    # Create a new model
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+
 class CyclicLRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __init__(self, learning_rate: float, batch_size: int, total_size: int, decay_rate: float = 0.5,
                  decay_steps: int = 5, warmup_epochs: int = 5, min_lr: float = 1e-6):

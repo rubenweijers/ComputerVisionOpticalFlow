5baa76b0a54730818e1fbff82ee651dfd2516fda
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-04-15 12:26:02 +0200

    Cacheing via disk

--- train_models_twostream.py
+++ train_models_twostream.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 
 from tensorflow.keras.callbacks import TensorBoard
@@ -18,12 +19,24 @@ if __name__ == "__main__":
     learning_rate = 0.01
     batch_size = 64
     epochs = 15
+    lr_schedule = "decay"
+    opt = "adam"
 
     model_variation = "model4"  # Fusion model
     resize = (112, 112)  # Make all images the same size
 
     # Load the data from the HMDB dataset
-    X_train, y_train, X_val, y_val, X_test, y_test = preprocess_data_hmdb(model_variation=model_variation, resize=resize)
+    if not os.path.exists(f"./data/{model_variation}_{resize[0]}.pickle"):
+        X_train, y_train, X_val, y_val, X_test, y_test = preprocess_data_hmdb(model_variation=model_variation, resize=resize)
+
+        # Save the data to disk
+        with open(f"./data/{model_variation}_{resize[0]}.pickle", "wb") as f:
+            pickle.dump((X_train, y_train, X_val, y_val, X_test, y_test), f)
+    else:
+        # Load the data from disk
+        with open(f"./data/{model_variation}_{resize[0]}.pickle", "rb") as f:
+            X_train, y_train, X_val, y_val, X_test, y_test = pickle.load(f)
+
     print(f"{X_train[0].shape=}; {X_train[1].shape=}; {y_train.shape=};\n"
           f"{X_val[0].shape=}; {X_val[1].shape=}; {y_val.shape=};\n"
           f"{X_test[0].shape=}; {X_test[1].shape=}; {y_test.shape=};")
@@ -41,7 +54,9 @@ if __name__ == "__main__":
     plot_model(model, to_file=f"./models/twostream.png", show_shapes=True, show_layer_names=True)
 
     # Prepare the model
-    model = prepare_model(model, learning_rate=learning_rate, batch_size=batch_size, total_size=total_size)
+    model = prepare_model(model, learning_rate=learning_rate, batch_size=batch_size, total_size=total_size,
+                          lr_schedule=lr_schedule, opt=opt)
+
     model.summary()
 
     tensorboard_callback = TensorBoard(log_dir=f"./logs/{model_variation}")

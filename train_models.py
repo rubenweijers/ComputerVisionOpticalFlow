f0b93bf7024af6fb1d9eb4bbf4922c3f13899c9d
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-04-15 15:48:32 +0200

    Improved params

--- train_models.py
+++ train_models.py
@@ -1,14 +1,11 @@
 import pickle
-import numpy as np
+
 import cv2
+import numpy as np
 from keras.utils import to_categorical
-
-
 from tensorflow.keras.callbacks import TensorBoard
 
 from models import make_model, prepare_model
-
-
 
 # Load data from pickle file
 with open("data/Stanford40.pickle", "rb") as f:
@@ -56,13 +53,15 @@ y_test = test_labels
 x_val = val_images
 y_val = val_labels
 
+
 def encode_labels(y_train, y_val, y_test):
     label_map = {label: idx for idx, label in enumerate(np.unique(y_train))}
     y_train = np.array([label_map[label] for label in y_train])
     y_val = np.array([label_map[label] for label in y_val])
     y_test = np.array([label_map[label] for label in y_test])
-    
+
     return (to_categorical(y_train), to_categorical(y_val), to_categorical(y_test))
+
 
 y_train, y_val, y_test = encode_labels(y_train, y_val, y_test)
 
@@ -70,51 +69,42 @@ y_train, y_val, y_test = encode_labels(y_train, y_val, y_test)
 if __name__ == "__main__":
     # Load the data from Stanford40.pickle
 
-
     print(f"{x_train.shape=}; {y_train.shape=}; {x_val.shape=}; {y_val.shape=}; {x_test.shape=}; {y_test.shape=}")
-    
+
     kernel_sizes = (7, 5, 3)
     pool_sizes = (2, 2, 2)
     filter_sizes = (32, 64, 128)
-    pooling_type = "avg"
-    dropout_value = None
+    dense_sizes = (1024, 512)
+    pooling_type = "max"
+    dropout_value = 0.5
     conv_act = "relu"
     normalise = False
 
-    learning_rate = 0.01
+    learning_rate = 0.001
     batch_size = 64
     total_size = x_train.shape[0]
     epochs = 15
+    lr_schedule = "decay"
+    opt = "adam"
 
-    model_variation = "averagejoe"  # Choose from: {baseline, nike, collegedropout, normaliser2000, averagejoe}
-
-    if model_variation == "baseline":
-        pass  # Default values are already set
-    elif model_variation == "nike":
-        conv_act = "swish"
-    elif model_variation == "collegedropout":
-        dropout_value = 0.5
-    elif model_variation == "normaliser2000":
-        normalise = True
-    elif model_variation == "averagejoe":
-        pooling_type = "avg"
-    else:
-        raise ValueError(f"Invalid model variation: {model_variation}")
+    model_variation = "model1"
 
     # Create the model
     model = make_model(kernel_sizes=kernel_sizes, pool_sizes=pool_sizes, pooling_type=pooling_type,
+                       dense_sizes=dense_sizes, filter_sizes=filter_sizes, normalise=normalise,
                        dropout_value=dropout_value, conv_act=conv_act)
 
     # Prepare the model
-    model = prepare_model(model, learning_rate=learning_rate, batch_size=batch_size, total_size=total_size)
+    model = prepare_model(model, learning_rate=learning_rate, batch_size=batch_size, total_size=total_size,
+                          lr_schedule=lr_schedule, opt=opt)
 
     tensorboard_callback = TensorBoard(log_dir=f"./logs/{model_variation}")
-    history = model.fit(x_train, y_train, epochs=epochs, validation_data=(
-        x_val, y_val), batch_size=batch_size, callbacks=[tensorboard_callback])
-    model.save(f"./models/model_{model_variation}.h5")
+    history = model.fit(x_train, y_train, epochs=epochs, validation_data=(x_val, y_val),
+                        batch_size=batch_size, callbacks=[tensorboard_callback])
+    model.save(f"./models/{model_variation}.h5")
 
     # Save the history
-    with open(f"./history/history_{model_variation}.pkl", "wb") as f:
+    with open(f"./history/{model_variation}.pkl", "wb") as f:
         pickle.dump(history.history, f)
 
     # Evaluate the model

8dc2e8201972c2a4569fc06e77b9b3f30f98687a
ruben <rubenweijers29@gmail.com>
2023-04-15 17:56:37 +0200

    AUGMENTATION

--- train_models.py
+++ train_models.py
@@ -1,8 +1,9 @@
 import pickle
-
+import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 from keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import TensorBoard
 
 from models import make_model, prepare_model
@@ -65,6 +66,41 @@ def encode_labels(y_train, y_val, y_test):
 
 y_train, y_val, y_test = encode_labels(y_train, y_val, y_test)
 
+# Create image data generator with augmentation options
+train_datagen = ImageDataGenerator(rotation_range=20,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
+# Fit the data generator to the training data
+train_datagen.fit(x_train)
+
+# Use the generator to create augmented image batches
+train_generator = train_datagen.flow(x_train, y_train, batch_size=64)
+
+# Define a function to plot the train and validation accuracy and loss graphs
+def plot_acc_loss(history):
+    # Plot the train and validation accuracy
+    plt.plot(history.history['accuracy'], label='train_acc')
+    plt.plot(history.history['val_accuracy'], label='val_acc')
+    plt.title('Model Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+    # Plot the train and validation loss
+    plt.plot(history.history['loss'], label='train_loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.title('Model Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
 
 if __name__ == "__main__":
     # Load the data from Stanford40.pickle
@@ -87,7 +123,7 @@ if __name__ == "__main__":
     lr_schedule = "decay"
     opt = "adam"
 
-    model_variation = "model1"
+    model_variation = "model1_augmented"
 
     # Create the model
     model = make_model(kernel_sizes=kernel_sizes, pool_sizes=pool_sizes, pooling_type=pooling_type,
@@ -99,14 +135,27 @@ if __name__ == "__main__":
                           lr_schedule=lr_schedule, opt=opt)
 
     tensorboard_callback = TensorBoard(log_dir=f"./logs/{model_variation}")
+
+    history = model.fit(train_generator,
+                    steps_per_epoch=len(x_train)//batch_size,
+                    epochs=epochs,
+                    validation_data=(x_val, y_val),
+                    callbacks=[tensorboard_callback])
+
     history = model.fit(x_train, y_train, epochs=epochs, validation_data=(x_val, y_val),
                         batch_size=batch_size, callbacks=[tensorboard_callback])
+
     model.save(f"./models/{model_variation}.h5")
 
-    # Save the history
+    # # Save the history
     with open(f"./history/{model_variation}.pkl", "wb") as f:
         pickle.dump(history.history, f)
 
     # Evaluate the model
     loss, accuracy = model.evaluate(x_test, y_test)
     print(f"Loss: {loss:.2f}; Accuracy: {accuracy:.2f}")
+    nparams = model.count_params()
+    print(nparams)
+
+    # Plot the accuracy and loss graphs
+    plot_acc_loss(history)
